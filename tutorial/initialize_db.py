b859eca3a999aa9d32e0366ac17e2295969a9046
vinhky20 <nguyenvinhky26@gmail.com>
2021-12-20 22:34:24 +0700

    Edit...

--- tutorial/initialize_db.py
+++ tutorial/initialize_db.py
@@ -39,13 +39,40 @@ def main(argv=sys.argv):
     DBSession.configure(bind=engine)
     Base.metadata.create_all(engine)
     with transaction.manager:
-        modelDM = DanhMuc(id_dm=1, tendanhmuc='sofa')
+        sofa = DanhMuc(id_dm=1, tendanhmuc='sofa')
+        ban = DanhMuc(id_dm=2, tendanhmuc='bàn')
+        tranh = DanhMuc(id_dm=3, tendanhmuc='tranh')
+        guong = DanhMuc(id_dm=4, tendanhmuc='gương')
 
-        modelSP = SanPham(id_sp='test', tensanpham='test', donvitinh='test', id_dm=1)
+        DBSession.add(sofa)
+        DBSession.add(ban)
+        DBSession.add(tranh)
+        DBSession.add(guong)
 
-        modelNV = NhanVien(id_nv=1, tennhanvien='Vinh Ky', username='vinhkyne', password='vinhkyne')
+
+        test = SanPham(id_sp='test', tensanpham='test', donvitinh='test', id_dm=1)
+
+        S1 = SanPham(id_sp='S1', tensanpham='Sofa mềm', donvitinh='bộ', id_dm=1)
+        S2 = SanPham(id_sp='S2', tensanpham='Sofa dài', donvitinh='bộ', id_dm=1)
+
+        B1 = SanPham(id_sp='B1', tensanpham='Bàn tròn', donvitinh='cái', id_dm=2)
+        B2 = SanPham(id_sp='B2', tensanpham='Bàn xoay', donvitinh='cái', id_dm=2)
+
+        T1 = SanPham(id_sp='T1', tensanpham='Tranh trừu tượng', donvitinh='bức', id_dm=3)
+        T2 = SanPham(id_sp='T2', tensanpham='Tranh thiếu nữ', donvitinh='bức', id_dm=3)
+
+        G1 = SanPham(id_sp='G1', tensanpham='Gương treo tường', donvitinh='cái', id_dm=4)
+
+        DBSession.add(test)
+        DBSession.add(S1)
+        DBSession.add(S2)
+        DBSession.add(T1)
+        DBSession.add(T2)
+        DBSession.add(B1)
+        DBSession.add(B2)
+        DBSession.add(G1)
 
 
-        DBSession.add(modelDM)
-        DBSession.add(modelSP)
-        DBSession.add(modelNV)
+        NV = NhanVien(id_nv=1, tennhanvien='Vinh Ky', username='vinhky', password='vinhky')
+
+        DBSession.add(NV)
